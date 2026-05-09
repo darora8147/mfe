6a178a3be1ec6700b9dd57bea1eaa1eb16cc1f37
darora8147 <divyamarora95@gmail.com>
2026-05-10 00:04:02 +0530

    Routing Enabled
    Communication between Container and Host

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,25 +1,28 @@
-import React from "react"
-import { Route, BrowserRouter, Switch } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import React from "react";
+import { Route, Router, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'ma'
-})
+  productionPrefix: "ma",
+});
 
-export default () => {
-    return (
-        <div>
-            <StylesProvider generateClassName={generateClassName}>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path="/" component={Landing} />
-                        <Route exact path="/pricing" component={Pricing} />
-                    </Switch>
-                </BrowserRouter>
-            </StylesProvider>
-        </div>
-    )
-}
+export default ({ history }) => {
+  return (
+    <div>
+      <Router history={history}>
+        <StylesProvider generateClassName={generateClassName}>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/pricing" component={Pricing} />
+          </Switch>
+        </StylesProvider>
+      </Router>
+    </div>
+  );
+};
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,20 +1,36 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if(onNavigate) {
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     );
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 }
 
 // We are running this file in development and in isolation. Mount the app immediately
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
